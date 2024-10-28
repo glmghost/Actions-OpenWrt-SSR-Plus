e0979480254ad55f821c9a16e46ae16c386d2463
glmghost <62704244+glmghost@users.noreply.github.com>
2024-10-28 20:54:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
