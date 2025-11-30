e9b545bee2517c4fe7a942a291154a8ff037be21
glmghost <62704244+glmghost@users.noreply.github.com>
2025-11-30 16:13:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,5 @@
 #echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+#sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+sed -i '$a src-git kwpackages https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
