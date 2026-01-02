d1892b67f42d5f0caa611939e52c1052db6f7a20
glmghost <62704244+glmghost@users.noreply.github.com>
2026-01-02 17:58:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,10 @@
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 sed -i '$a src-git kwpackages https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-sed -i '$a src-git naiveproxy https://github.com/klzgrad/naiveproxy.git' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+#sed -i '$a src-git naiveproxy https://github.com/klzgrad/naiveproxy.git' feeds.conf.default
