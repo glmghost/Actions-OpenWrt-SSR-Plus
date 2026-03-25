f344d4fa56ed39ab64c27013629dfcda6152a3fe
glmghost <62704244+glmghost@users.noreply.github.com>
2026-03-25 19:48:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -47,6 +47,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
+#echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
