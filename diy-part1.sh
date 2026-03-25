2c6b38eee9b43dda967d90a1610414d4330078f1
glmghost <62704244+glmghost@users.noreply.github.com>
2026-03-25 19:36:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,8 +23,30 @@
 #echo '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #echo '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
+#sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 #sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 #sed -i '$a src-git oppackages https://github.com/kiddin9/op-packages.git' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 #sed -i '$a src-git naiveproxy https://github.com/klzgrad/naiveproxy.git' feeds.conf.default
+
+
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
