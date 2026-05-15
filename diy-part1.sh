ead6cb952a26bf60da5c86ff16894c00a544511b
glmghost <62704244+glmghost@users.noreply.github.com>
2026-05-15 16:05:51 +0800

    Update feed sources in diy-part1.sh
    Comment out helloworld feed source and add smpackage.

--- diy-part1.sh
+++ diy-part1.sh
@@ -49,5 +49,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git jell https://github.com/kenzok8/jell' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#sed -i "/helloworld/d" "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
